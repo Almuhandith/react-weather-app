2cffa9496c841df3fe25612fb426216bbccea290
Almuhandith <almuhandith1497@gmail.com>
2023-01-30 05:16:39 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '11ed70e547msh5d5596db92b1a0fp1980a1jsn3ee3052cc4c3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
